d168972abd9607730c4bc2347cfadc4b32940bfb
BJB2088 <BJB2088@gmail.com>
2014-05-02 09:52:02 -0700

    looking at time

--- GettingData.R
+++ GettingData.R
@@ -1,3 +1,7 @@
+# Are there any airlines with flight delays that are systematically worse
+# than other airlines? Please examine this using 25 years of flight data for
+# the entire country. 
+
 # --- setting up parameters to access the data base --- #
 endpoint <- "flights.cwick.co.nz"
 user <- "student"
@@ -19,6 +23,13 @@ str(flights)
 head(flights) 
 # or you could use tbl_df to print it pretty
 tbl_df(head(flights))
-delay=flights %.% select(year:dayofweek,uniquecarrier,arrdelay,depdelay,carrierdelay:lateaircraftdelay)
+delay=flights %.% select(year:dayofweek,uniquecarrier,arrdelay,depdelay,carrierdelay:lateaircraftdelay,origin:dest)
 tbl_df(head(delay))
-
+# Getting number of flights by carrier
+num=delay %.% group_by(uniquecarrier) %.% summarise(num=n())
+# Getting one carrier 
+PS = as.data.frame(delay %.% filter(uniquecarrier=="PS"))
+head(PS)
+PS$date=paste(PS$dayofmonth,"-",PS$month,"-",PS$year,sep="")
+dmy(PS$date)
+? ISOdate
