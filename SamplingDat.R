68b25032738ecfb577f93358a966629ccc7de327
BJB2088 <BJB2088@gmail.com>
2014-05-05 13:04:35 -0700

    start sampling script

---
+++ SamplingDat.R
@@ -0,0 +1,27 @@
+library(dplyr)
+library(RPostgreSQL)
+library(ggplot2)
+
+# --- setting up parameters to access the data base --- #
+endpoint <- "flights.cwick.co.nz"
+user <- "student"
+password <- "password"
+
+ontime <- src_postgres("ontime", 
+                       host = endpoint,
+                       port = 5432,
+                       user = user,
+                       password = password)
+# -------- #
+
+flights <- tbl(ontime, "flights")
+#' `flights` is special object, it points to the remote database, but
+#' you can work with it in dplyr like it is a data.frame.
+str(flights)
+
+#' This isn't your usual head function, but it acts the same way:
+head(flights) 
+# or you could use tbl_df to print it pretty
+tbl_df(head(flights))
+delay=flights %.% select(year:dayofweek,uniquecarrier,arrdelay,depdelay,carrierdelay:lateaircraftdelay,origin:dest)
+tbl_df(head(delay))
