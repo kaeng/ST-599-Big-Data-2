229a24e56939237a200b5c283095b10d8233ce3a
kaeng <engka@science.oregonstate.edu>
2014-05-05 15:19:39 -0700

    Sampling Code (assign carriers to group members)

--- SamplingDat.R
+++ SamplingDat.R
@@ -17,11 +17,51 @@ ontime <- src_postgres("ontime",
 flights <- tbl(ontime, "flights")
 #' `flights` is special object, it points to the remote database, but
 #' you can work with it in dplyr like it is a data.frame.
-str(flights)
+#str(flights)
 
 #' This isn't your usual head function, but it acts the same way:
-head(flights) 
+#head(flights) 
 # or you could use tbl_df to print it pretty
-tbl_df(head(flights))
+#tbl_df(head(flights))
 delay=flights %.% select(year:dayofweek,uniquecarrier,arrdelay,depdelay,carrierdelay:lateaircraftdelay,origin:dest)
-tbl_df(head(delay))
+#tbl_df(head(delay))
+
+
+#########
+# getting data set and stratum stats calculations
+#########
+
+# have EnvKatie.RData loaded
+
+carriers2013 <- unique(Avgs_by_month_local$uniquecarrier)
+
+get2013dat <- delay %.% 
+  select(year,uniquecarrier,arrdelay,depdelay) %.%
+  filter(uniquecarrier == "US") %.% filter(random() < .001) %.% summarise(avgarrdelay = mean(arrdelay),
+                                                                          stratsize = n())
+
+#|
+#           uniquecarrier == "MQ" |  #KT
+#           uniquecarrier == "YV" |  #KT
+#           uniquecarrier == "AS" |  #KT
+#           uniquecarrier == "F9" |  #LW
+#           uniquecarrier == "DL" |  #LW
+#           uniquecarrier == "UA" |  #LW
+#           uniquecarrier == "AA" |  #LW
+#           uniquecarrier == "HA" |  #BB
+#           uniquecarrier == "VX" |  #BB
+#           uniquecarrier == "B6" |  #BB
+#           uniquecarrier == "OO" |  #BB
+#           uniquecarrier == "FL" |  #FW
+#           uniquecarrier == "9E" |  #FW
+#           uniquecarrier == "EV" |  #FW
+#           uniquecarrier == "WN")   #FW
+#%.% group_by(uniquecarrier)
+
+  
+explain(get2013dat)
+
+
+sample2013 <- collect(get2013dat)
+
+
