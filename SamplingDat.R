46bec919719e5863f65b8f4a491a6ca5fca887e4
wfw119 <wfw119@gmail.com>
2014-05-05 21:26:15 -0700

    Update SamplingDat.R

--- SamplingDat.R
+++ SamplingDat.R
@@ -86,5 +86,33 @@ for (i in c("US","MQ","YV","AS","F9","DL","UA","AA","HA","VX","B6","OO","FL","9E
 ##############################
 # FangWu's Code
 ##############################
+# getting sample data and analyzing
 
+getdat_FL <- delay %.% 
+  select(year,uniquecarrier,arrdelay,depdelay) %.%
+  filter(uniquecarrier == "FL") %.% filter(random() < .001) %.% 
+  summarise(sampleavg = mean(arrdelay), samplevar = var(arrdelay), samplesize = n())
+
+sampledat_FL <- collect(getdat_FL)
+
+getdat_9E <- delay %.% 
+  select(year,uniquecarrier,arrdelay,depdelay) %.%
+  filter(uniquecarrier == "9E") %.% filter(random() < .001) %.% 
+  summarise(sampleavg = mean(arrdelay), samplevar = var(arrdelay), samplesize = n())
+
+sampledat_9E <- collect(getdat_9E)
+
+getdat_EV <- delay %.% 
+  select(year,uniquecarrier,arrdelay,depdelay) %.%
+  filter(uniquecarrier == "EV") %.% filter(random() < .001) %.% 
+  summarise(sampleavg = mean(arrdelay), samplevar = var(arrdelay), samplesize = n())
+
+sampledat_EV <- collect(getdat_EV)
+
+getdat_WN <- delay %.% 
+  select(year,uniquecarrier,arrdelay,depdelay) %.%
+  filter(uniquecarrier == "WN") %.% filter(random() < .001) %.% 
+  summarise(sampleavg = mean(arrdelay), samplevar = var(arrdelay), samplesize = n())
+
+sampledat_WN <- collect(getdat_WN)
 
